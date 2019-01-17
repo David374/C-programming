d9d09a570ab3a65184315d2d1e82b0f3494fa867
David374 <38322139+David374@users.noreply.github.com>
2019-01-17 20:39:03 +0300

    Create main.c

---
+++ 4_4_6/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  short int N, endline, ToEndline;
+  endline = 1;
+  ToEndline = 1;
+  scanf("%d", &N);
+  if((N < 1) || (N > 9))
+  {
+  	printf("N is wrong: %d", N);
+  	exit;
+  }
+  else
+  	while(endline <= N)
+  	{
+  		while(ToEndline < endline)
+  		{
+  			if(ToEndline != 0 && ToEndline != endline + 1)
+  				printf(" ");
+  			ToEndline++;
+  			printf("%d", ToEndline);
+  		}
+  		if(N != endline)
+  		{
+  			printf("\n");
+  		}
+  		ToEndline = 0;
+  		endline++;
+  	}
+  	
+  	return (0);
+}
