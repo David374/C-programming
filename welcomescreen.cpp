38b219269b85eecf8c9de4c52a60ab2db249d82f
David374 <38322139+David374@users.noreply.github.com>
2019-04-08 17:16:10 +0300

    Update welcomescreen.cpp

--- welcomescreen.cpp
+++ welcomescreen.cpp
@@ -22,3 +22,27 @@ int main()
 	return 0;
 
 }
+
+
+---------------------------------------
+#include "pch.h"
+#include <iostream>
+#include <locale>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	SetWindowPos(GetConsoleWindow(), NULL, 100, 100, 600, 300, NULL);
+	setlocale(LC_ALL, "Russian");
+	cout << " |--------------------------------------------------------------------------------------------|" << endl;
+	cout << " |                                                                                            |" << endl;
+	cout << " |                                                                                            |" << endl;
+	cout << " |             Добро пожаловать в наш отель! Желаете забронировать номер?                     |" << endl;
+	cout << " |                                                                                            |" << endl;
+	cout << " |                                                                                            |" << endl;
+	cout << " |--------------------------------------------------------------------------------------------|" << endl;
+
+	return 0;
+
+}
