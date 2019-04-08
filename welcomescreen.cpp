5275ac82d2c8999d7c65cc3fcd998281e8f8f862
David374 <38322139+David374@users.noreply.github.com>
2019-04-08 17:03:31 +0300

    Create welcomescreen.cpp

---
+++ welcomescreen.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include <iostream>
+#include <locale>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+	HANDLE hWnd = GetStdHandle(STD_OUTPUT_HANDLE);
+	COORD bufferSize = { 101, 100 };
+	SetConsoleScreenBufferSize(hWnd, bufferSize);
+	
+	setlocale(LC_ALL, "Russian");
+	cout << "|--------------------------------------------------------------------------------------------|" << endl;
+	cout << "|                                                                                            |" << endl;
+	cout << "|                                                                                            |" << endl;
+	cout << "|             Добро пожаловать в наш отель! Желаете забронировать номер?                     |" << endl;
+	cout << "|                                                                                            |" << endl;
+	cout << "|                                                                                            |" << endl;
+	cout << "|--------------------------------------------------------------------------------------------|" << endl;
+
+	return 0;
+
+}
