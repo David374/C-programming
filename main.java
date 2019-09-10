54f5b4d5ccbb40ad2e96dab796629d0cd0b82bf7
David374 <38322139+David374@users.noreply.github.com>
2019-09-10 11:45:13 +0300

    Update main.java

--- main.java
+++ main.java
@@ -4,17 +4,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        try{
-	Droby D1 = new Droby(1, 2);
-	Droby D2 = new Droby(3, 4);
-	System.out.println(D1.outDroby());
-	System.out.println(D2.outDroby());
-	D1.addTwoDroby(D2);
-	System.out.println(D1.outDroby());
-	D1.addTwoDroby(D2);
+        try {
+            Droby D1 = new Droby(1, 2);
+            Droby D2 = new Droby(3, 4);
+            System.out.println(D1.outDroby());
+            System.out.println(D2.outDroby());
+            D1.addTwoDroby(D2);
+            System.out.println(D1.outDroby());
+            D2.addTwoDroby(D1);
+            System.out.println(D2.outDroby());
 
-    } catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
+    }
 }
-
