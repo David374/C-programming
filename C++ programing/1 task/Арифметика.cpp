44a03d8f7acbe38142a45768fe98982f4731f740
David374 <38322139+David374@users.noreply.github.com>
2019-09-14 18:13:13 +0300

    Update Арифметика.cpp

--- C++ programing/1 task/Арифметика.cpp
+++ C++ programing/1 task/Арифметика.cpp
@@ -28,3 +28,66 @@ int main()
 }
 *********************************************
 2. version
+#include <iostream>
+
+using namespace std;
+
+double Sum(double a, double b);
+double Difference(double a, double b);
+double Multiply(double a, double b);
+double Div(double a, double b);
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	double a, b;
+	cout << "Введите 1-е число:";
+	cin >> a;
+	cout << "Введите 2-е число:";
+	cin >> b;
+	Sum(a, b);
+	Difference(a, b);
+	Multiply(a, b);
+	if (b != 0)
+	{
+		Div(a, b);
+	}
+	else
+	{
+		cout << "Невозможно найти частное, так как делитель равен нулю!/На ноль делить нельзя!";
+	}
+}
+
+double Sum(double a, double b) 
+{
+	double result = a + b;
+	cout << "Сумма данных чисел = " << result << endl;
+
+	return 0;
+}
+
+double Difference(double a, double b)
+{
+	double result = a - b;
+	cout << "Разность данных чисел = " << result << endl;
+
+	return 0;
+}
+
+double Multiply(double a, double b)
+{
+	double result = a * b;
+	cout << "Произведение данных чисел = " << result << endl;
+
+	return 0;
+}
+
+double Div(double a, double b)
+{
+	double result = a / b;
+	cout << "Частное данных чисел = " << result << endl;
+
+	return 0;
+}
+*********************************************
+3. version
